50ccfbae8a43b56e42a0fd197c5ce76f5ea733fd
Alejandro Montes <alejandro.montes.g@gmail.com>
2013-12-16 15:38:22 +0100

    Added logistic regression exercises

---
+++ week 3/costFunction.m
@@ -0,0 +1,12 @@
+function [J, grad] = costFunction(theta, X, y)
+%COSTFUNCTION Compute cost and gradient for logistic regression
+%   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
+%   parameter for logistic regression and the gradient of the cost
+%   w.r.t. to the parameters.
+
+m = length(y);
+hypothesis = sigmoid(X * theta);
+J = (-y' * log(hypothesis) - (1 - y)' * log(1 - hypothesis)) / m;
+grad = ((hypothesis - y)' * X) / m;
+
+end
